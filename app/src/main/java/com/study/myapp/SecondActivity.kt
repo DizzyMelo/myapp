eb2fbe119a013fc0d1afed4935161f2c17a1a454
Daniel Melo <daniel.melo42@outlook.com>
2022-10-10 21:51:56 -0600

    second activity added

---
+++ app/src/main/java/com/study/myapp/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.study.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
